d36a992c95b4c0ce9f205d4d01a1f4f0a847a283
Sarah Nosal <nosalsm@gmail.com>
2022-02-03 13:38:45 -0600

    got bonus working

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,8 +2,8 @@ class Client < ApplicationRecord
     has_many :memberships
     has_many :gyms, through: :memberships
 
-    def sum
-        charges = self.memberships.pluck(:charge)
-        charges.sum()
-    end
+    # def sum
+    #     charges = self.memberships.pluck(:charge)
+    #     charges.sum()
+    # end
 end
--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -1,6 +1,12 @@
 class ClientSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age
+  attributes :id, :name, :age, :sum
 
   has_many :memberships
   has_many :gyms
+
+  def sum
+    
+    charges = object.memberships.pluck(:charge)
+    charges.sum()
+end
 end
